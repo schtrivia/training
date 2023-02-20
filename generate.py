f6f9c19f4a0f9a96a317435a2fb28c2915eacfe7
Alexey <alekami649@yandex.ru>
2023-02-20 21:31:40 +0700

    math

--- generate.py
+++ generate.py
@@ -1,21 +1,13 @@
 import glob
 
-pattern = "*.json"
+pattern = "*.jsonl"
 
-output_file = "main.json"
+output_file = "main.jsonl"
 
-with open(output_file, "w") as outfile:
-
+with open(output_file, "w", encoding="utf-8") as outfile:
     for filename in glob.glob(pattern):
-
         if filename != output_file:
-
-            with open(filename, "r") as infile:
-
+            with open(filename, "r", encoding="utf-8") as infile:
                 text = infile.read()
-
                 outfile.write(text)
-
                 outfile.write("\n")
-
-
